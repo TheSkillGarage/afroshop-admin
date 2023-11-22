6049f529929ee9685827ca9a03b5ab403e7c3f13
Ediong Alexander <ediongalex@gmail.com>
2023-11-22 17:25:00 +0100

    Chore: Made sidebar sticky

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -7,7 +7,7 @@ import SIDEBAR_TABS from "../../data/sidebar-tabs";
 const AdminSidebar = () => {
 
     return (
-        <aside className="px-6 flex flex-col w-[266px] min-h-[calc(1024px-69px)] max-h-[calc(1024px-69px)] border-r border-1 border-[#E6E6E6] justify-between bg-[#ffffff] mb-6">
+        <aside className="px-6 flex flex-col w-[266px] min-h-screen max-h-screen border-r border-1 border-[#E6E6E6] justify-between bg-[#ffffff] sticky top-0 mb-4">
             <div className="mt-10 flex flex-col gap-1">
                 {
                     SIDEBAR_TABS.map(({name, path}, index) => {
