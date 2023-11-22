debef2ae0254fe3da819bce4abd058664cf20548
Ediong Alexander <ediongalex@gmail.com>
2023-11-22 12:00:54 +0100

    Chore: Adjusted table dimensions

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -7,8 +7,8 @@ import SIDEBAR_TABS from "../../data/sidebar-tabs";
 const AdminSidebar = () => {
 
     return (
-        <aside className="px-6 flex flex-col w-[266px] h-[1024px] border-r border-1 border-[#E6E6E6] justify-between bg-[#ffffff] mb-6">
-            <div className="mt-8 flex flex-col gap-2">
+        <aside className="px-6 flex flex-col w-[266px] min-h-[calc(1024px-69px)] max-h-[calc(1024px-69px)] border-r border-1 border-[#E6E6E6] justify-between bg-[#ffffff] mb-6">
+            <div className="mt-10 flex flex-col gap-1">
                 {
                     SIDEBAR_TABS.map(({name, path}, index) => {
                         return (
