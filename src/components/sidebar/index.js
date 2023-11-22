7b1d35f40dee5e0b80d36b5f100c89749e0735d7
Eshy10 <adewalebose15@gmail.com>
2023-11-22 23:20:12 +0100

    chore: sidebar fix

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -7,7 +7,7 @@ import SIDEBAR_TABS from "../../data/sidebar-tabs";
 const AdminSidebar = () => {
 
     return (
-        <aside className="px-6 flex flex-col w-[266px] min-h-screen max-h-screen border-r border-1 border-[#E6E6E6] justify-between bg-[#ffffff] sticky top-0 mb-4">
+        <aside className="px-6 flex flex-col w-[266px] min-h-screen max-h-screen border-r border-1 border-[#E6E6E6] justify-between bg-[#ffffff] sticky top-0 pb-4">
             <div className="mt-10 flex flex-col gap-1">
                 {
                     SIDEBAR_TABS.map(({name, path}, index) => {
