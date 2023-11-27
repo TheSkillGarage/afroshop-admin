9d66b21834efda2c0e8fd7313c01ab41c9eb5be8
Ediong Alexander <ediongalex@gmail.com>
2023-11-27 23:30:01 +0100

    Feat: Filter Modal UI close

--- src/components/orders/filter-modal/index.js
+++ src/components/orders/filter-modal/index.js
@@ -1,16 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { LeftArrow } from "../../../images";
 import Filter from "./filter";
 
 
-const FilterModal = () => {
+const FilterModal = ({ setOpenFilter }) => {
     const [isOpen, setIsOpen] = useState(0);
 
     const handleOpen = (key) => setIsOpen(key);
 
+    const dropdownRef = useRef(null);
+
+    const handleClick = (event) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+            setOpenFilter(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener("click", handleClick);
+
+        return () => {
+            document.removeEventListener("click", handleClick);
+        };
+    }, []);
+
     return (
         <div className="flex justify-end fixed inset-0 bg-[rgba(0,0,0,0.2)] z-50">
-            <div className="w-[595px] p-6 min-h-screen rounded shadow-lg bg-[#ffffff]">
+            <div className="w-[595px] p-6 min-h-screen rounded shadow-lg bg-[#ffffff] filter-modal" ref={dropdownRef}>
                 <div className="flex gap-3 items-center">
                     <LeftArrow />
                     <p className="font-bold py-1 text-[#186F3D] text-[20px] leading-[32px]">Filter(s)</p>
@@ -18,7 +34,7 @@ const FilterModal = () => {
 
                 {
                     [1, 2, 3, 4, 5].map((item, key) => {
-                        return <Filter key={key} id={item} isOpen={isOpen === key} handleOpen={handleOpen} item={key}/>
+                        return <Filter key={key} id={item} isOpen={isOpen === key} handleOpen={handleOpen} item={key} />
                     })
                 }
                 <div className="flex justify-end gap-4 mt-20">
--- src/components/search/index.js
+++ src/components/search/index.js
@@ -17,13 +17,13 @@ const Search = ({ handleSearch }) => {
 
     return (
         <div className="flex justify-between items-center px-4 py-6 h-[93px]">
-           {openFilter && <FilterModal />}
+           {openFilter && <FilterModal setOpenFilter={setOpenFilter}/>}
             <form action="" className="w-[514px] relative" onSubmit={handleSubmit}>
                 <SearchIcon className="absolute top-[10px] left-[18px] " />
                 <input type="text" name="search" id="tableSearch" placeholder="Text" className="bg-[#F2F2F2] w-full h-[45px] rounded-[30px] text-[#999999] px-12 focus:outline-none" onChange={(e) => handleChange(e)} />
             </form>
 
-            <div className="w-[108px] h-[44px] rounded border border-[0.5px] flex items-center justify-center gap-2 cursor-pointer" onClick={() => setOpenFilter(!openFilter)}>
+            <div className="w-[108px] h-[44px] rounded border border-[0.5px] flex items-center justify-center gap-2 cursor-pointer" onClick={(e) => {e.stopPropagation(); setOpenFilter(!openFilter);}}>
                 <p className="text-[16px] leading-[24px] text-[#333333]">Filter</p>
                 <FilterIcon />
             </div>
