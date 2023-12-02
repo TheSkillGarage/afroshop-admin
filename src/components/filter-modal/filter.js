3707178355315f08b0ab33e42e921ea765c5932d
Ediong Alexander <ediongalex@gmail.com>
2023-12-02 22:46:50 +0100

    Search functionality for filters

--- src/components/filter-modal/filter.js
+++ src/components/filter-modal/filter.js
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NextIcon, SearchIcon } from "../../images";
 import CustomScrollbar from "./filter.styles";
 
 const Filter = ({ toggleFilters, index, item, handleToggleFilters, DATA, handleChange }) => {
 
-    const uniqueValues = Array.from(
-        new Set(
+    const [uniqueValues, setUniqueValues] = useState([]);
+
+    const uniqueArray = Array.from(
+          new Set(
             DATA.map(obj => {
-                // price is an object containing paymentMethod and price
-                if (item === 'price' && obj[item] && typeof obj[item] === 'object') {
-                    return obj[item].price;
-                }
-                // For other keys, directly return the value
-                return obj[item];
-            })
-        )
-    );
+              if (item === 'price' && obj[item] && typeof obj[item] === 'object') {
+                return obj[item].price;
+              }
+              return obj[item];
+            }).filter(value => value !== undefined)
+          )
+        );
+      
+    
+    useEffect(() => {
+      setUniqueValues(uniqueArray);
+    }, []);
+    
+    const searchUniqueValues = (e) => {
+      let val = e.target.value;
+    
+      let uniqueItems = uniqueArray.filter(text => text.toLowerCase().includes(val.toLowerCase()));
+    
+      setUniqueValues(uniqueItems);
+    };
+    
 
     return (
         <div className="mt-6">
@@ -27,7 +41,7 @@ const Filter = ({ toggleFilters, index, item, handleToggleFilters, DATA, handleC
                 <div className={`p-6 border border-1 border-[#E6E6E6] rounded h-fit ${toggleFilters[index] ? "" : "hidden"}`}>
                     <div className="relative mb-2">
                         <SearchIcon className="absolute top-[10px] left-[18px] " />
-                        <input type="text" name={`search-${item}`} id="tableSearch" placeholder="Text" className="bg-[#F2F2F2] w-full h-[45px] rounded-[30px] text-[#999999] px-12 focus:outline-none"/>
+                        <input type="text" name={`search-${item}`} id="tableSearch" placeholder="Text" className="bg-[#F2F2F2] w-full h-[45px] rounded-[30px] text-[#999999] px-12 focus:outline-none" onChange={(e) => searchUniqueValues(e)}/>
                     </div>
                     <CustomScrollbar className="max-h-[393px]">
                         <div className="mb-4">
