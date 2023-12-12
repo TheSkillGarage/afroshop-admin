34c957f1a5e010f1cbab40660867f003c3814791
Kara <abasiakaraalexander@gmail.com>
2023-12-12 09:30:48 +0100

    Support page

--- src/components/support/support.js
+++ src/components/support/support.js
@@ -5,37 +5,57 @@ import {
   PhoneSupport,
   EmailSupport,
 } from "../../images";
+import { Link } from "react-router-dom";
 
 const Support = () => {
   return (
-    <div>
-      <div className="bg-[#f2f2f2] border border-red-500">Support</div>
-      <div className="bg-white p-[50px]">
-        <div>
-          <div>Need Help?</div>
-          <div>Reach our to us on the following channels?</div>
+    <div className="w-[100%] mx-auto bg-[#F2F2F2]">
+      <div className="py-[12px]">
+        <div className="py-[8px] text-[13px] px-[30px] text-[#186F3D]">
+          <Link to="/support">Support</Link>
+        </div>
+      </div>
+      <div className="bg-white px-[24px] py-[50px] mx-[12px]">
+        <div className="w-[50%] mx-auto text-center">
           <div>
-            <img src={SupportIllustration} />
+            <div className="text-[#186F3D] text-[31px] font-bold">
+              Need Help?
+            </div>
+            <div className="text-[#333333] text-[16px] ">
+              Reach our to us on the following channels?
+            </div>
+            <div className="flex justify-center items-center pt-[20px]">
+              <img src={SupportIllustration} />
+            </div>
           </div>
         </div>
-        <div>
-          <div>
-            <div>
-              <img src={EmailSupport} />
-            </div>
-            <div></div>
-          </div>
-          <div>
-            <div>
-              <img src={PhoneSupport} />
-            </div>
-            <div></div>
-          </div>
-          <div>
+        <div className="flex mt-[20px] gap-[24px] w-[100%] justify-center">
+          <div className="w-[30%] py-[16px] px-[24px] flex flex-wrap gap-[24px] rounded-[4px] shadow-lg">
             <div>
               <img src={WhatsAppSupport} />
             </div>
-            <div></div>
+            <div>
+              <p className="font-bold text-[#333333]">Email us</p>
+              <p className="text-[#7F7F7F]">sales@afroshop.com</p>
+            </div>
+          </div>
+          <div className="w-[30%] py-[16px] px-[24px] flex flex-wrap gap-[24px] rounded-[4px] shadow-lg">
+            <div>
+              <img src={EmailSupport} />
+            </div>
+            <div>
+              <p className="font-bold text-[#333333]">Phone Number</p>
+              <p className="text-[#7F7F7F]">+127 000 000 1111</p>
+            </div>
+          </div>
+          <div className="w-[30%] py-[16px] px-[24px] flex flex-wrap gap-[24px] rounded-[4px] shadow-lg">
+            <div>
+              <img src={PhoneSupport} />
+            </div>
+            <div>
+              <p className="font-bold text-[#333333]">WhatsApp</p>
+              <p className="text-[#7F7F7F]">+127 000 000 1111</p>
+            </div>
           </div>
         </div>
       </div>
