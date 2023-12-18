0410623cbf8d5e15990b9127742f4a9555a2e81c
Eshy10 <adewalebose15@gmail.com>
2023-12-18 17:34:24 +0100

    chore: file rename in support component

--- src/components/index.js
+++ src/components/index.js
@@ -6,5 +6,5 @@ export { default as ProductsDashboard } from './products';
 export { default as ViewOrders } from './orders/view-orders';
 export {default as Products} from './addProduct/ProductImage'
 export {default as RolesAndPermissions} from './roles-and-permissions';
-export {default as Support} from './support/support';
+export {default as Support} from './support';
 export {default as Profile} from './profile';
