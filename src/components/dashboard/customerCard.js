e9bfc7e204afad6eaecaa2952d0d5a2fbb7ad2a0
RatiMathur <rmathur18@gmail.com>
2023-12-19 12:18:14 -0500

    top constumer ui

--- src/components/dashboard/customerCard.js
+++ src/components/dashboard/customerCard.js
@@ -10,17 +10,17 @@ const CustomerCard = ({
     <div className="flex gap-4">
       <img src={customerImage} alt="Customer Image" className='w-[20%]' />
 
-      <div className="flex gap-4 justify-between w-[70%]">
-        <div className="flex flex-col gap-1 flex-wrap w-full">
-          <p className="font-semibold text-[13px] text-[#186F3D] ">
+      <div className="flex gap-4 justify-between w-full">
+        <div className="flex flex-col gap-1 flex-wrap w-[60%]">
+          <p className="w-full font-semibold text-[13px] text-[#186F3D] ">
             {customerName}
           </p>
-          <p className="font-normal text-[13px] text-[#7F7F7F] truncate">
+          <p className="w-full font-normal text-[13px] text-[#7F7F7F] break-words">
             {customerEmail}
           </p>
         </div>
 
-        <p className="font-normal text-[13px] text-[#7F7F7F] pt-[15px] truncate">
+        <p className="font-normal text-[13px] text-[#7F7F7F] pt-[15px] break-words">
           {numberOrders}
         </p>
       </div>
