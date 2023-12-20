84c2ed96835ae422649880a75d98230d3b8a6255
Ediong Alexander <ediongalex@gmail.com>
2023-12-20 18:21:42 +0100

    Made changes to customer card

--- src/components/dashboard/customerCard.js
+++ src/components/dashboard/customerCard.js
@@ -7,20 +7,20 @@ const CustomerCard = ({
   numberOrders,
 }) => {
   return (
-    <div className="flex gap-4">
+    <div className="flex gap-2">
       <img src={customerImage} alt="Customer Image" className='w-[50px] h-[50px]'/>
 
-      <div className="flex gap-4 justify-between w-full">
-        <div className="flex flex-col gap-1 flex-wrap">
+      <div className="flex justify-between w-full items-center">
+        <div className="flex flex-col gap-1 flex-wrap w-[134px]">
           <p className="w-full font-semibold text-[13px] text-[#186F3D] ">
             {customerName}
           </p>
-          <p className="w-full font-normal text-[13px] text-[#7F7F7F] break-all">
+          <p className="w-full font-normal text-[13px] text-[#7F7F7F]">
             {customerEmail}
           </p>
         </div>
 
-        <p className="font-normal text-[13px] text-[#7F7F7F] pt-[15px] break-all">
+        <p className="font-normal text-[13px] text-[#7F7F7F] w-[70px]">
           {numberOrders}
         </p>
       </div>
