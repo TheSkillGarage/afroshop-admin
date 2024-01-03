3c53f7dba32fe816dfc2f13d1eb14a43fc0a4418
Eshy10 <adewalebose15@gmail.com>
2024-01-03 20:28:16 +0100

    fix

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ export { default as AdminNavbar } from './navbar';
 export { default as AdminSidebar } from './sidebar';
 export { default as OrdersDashboard } from './orders';
 export { default as ProductsDashboard } from './products';
+export { default as ViewOrders } from './orders/view-orders';
 export {default as RolesAndPermissions} from './roles-and-permissions';
 export {default as Welcome} from './dashboard/welcome'
 export {default as Dashboard} from './dashboard/dashboard'
