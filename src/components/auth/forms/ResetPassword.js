b966a54d6e79da35fadcc384178a8773e2723e61
Eshy10 <adewalebose15@gmail.com>
2024-01-08 19:43:14 +0100

    chore: add form

--- src/components/auth/forms/ResetPassword.js
+++ src/components/auth/forms/ResetPassword.js
@@ -17,20 +17,22 @@ const ResetPassword = () => {
       <p className="text-center text-[16px] text-[#ccc] pb-[24px]">
         We’ll email you a password reset link
       </p>
-      <InputComponent
-        inputType="email"
-        label="Email"
-        fieldName={"email"}
-        placeholder="Enter email"
-        leftIcon={IconMessage}
-        className="bg-[#F2F2F2]  border mb-[20px] border-[#186F3D]"
-        control={control}
-        errors={errors}
-        register={register}
-      />
-      <Button icon="white" className="w-[400px]">
-        Reset
-      </Button>
+      <form>
+        <InputComponent
+          inputType="email"
+          label="Email"
+          fieldName={"email"}
+          placeholder="Enter email"
+          leftIcon={IconMessage}
+          className="bg-[#F2F2F2]  border mb-[20px] border-[#186F3D]"
+          control={control}
+          errors={errors}
+          register={register}
+        />
+        <Button icon="white" className="w-[400px]">
+          Reset
+        </Button>
+      </form>
     </div>
   );
 };
