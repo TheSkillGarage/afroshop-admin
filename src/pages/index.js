0a25d4d9b66a9ce3623e93bfa2dcaad27e94a117
Ediong Alexander <ediongalex@gmail.com>
2024-01-08 14:19:23 +0100

    Fix: fixed merge error on routes

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ export { default as OverviewPage } from './App'
 export { default as Orders } from './orders-dashboard'
 export { default as Products } from './products-dashboard'
 export { default as ViewOrdersPage } from './view-orders'
-export { default as AddProducts } from './addProducts'
+export { default as AddProducts } from './AddProducts'
 export { default as RolesAndPermissionsDashboard } from "./roles-and-permissions/roles-and-permissions-dashboard";
 export { default as AddNewRole } from "./roles-and-permissions/add-new-role";
 export {default as DashboardPage} from './dashboardPage'
