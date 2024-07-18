04e6176a96e42797e968e1731ef0a32087628c27
ObiTam <45231955+ObiTam@users.noreply.github.com>
2024-07-18 18:54:35 +0100

    Update OrderSummaryFunctions.js

--- src/utils/OrderSummaryFunctions.js
+++ src/utils/OrderSummaryFunctions.js
@@ -156,8 +156,8 @@ export const getLineChartData = (selectedYear, ordersData, storeCreateDate) => {
       const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); // Convert milliseconds to days
       
       if (diffDays < 7 && diffDays >= 0) {
-        data.income[7 - diffDays] += order.grandTotal; // Use 6 - diffDays to match the correct index
-        data.orders[7 - diffDays] += 1; // Use 6 - diffDays to match the correct index
+        data.income[7 - diffDays] += order.grandTotal; // Use 7 - diffDays to match the correct index
+        data.orders[7 - diffDays] += 1; // Use 7 - diffDays to match the correct index
       }
     });
   }
